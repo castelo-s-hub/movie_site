d214e0dd6ba32a4eb610410a653624eb11977242
KAVI <kavindu123450@gmail.com>
2023-10-22 23:06:42 +0530

    home section 2

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   isMenuOpen: boolean = false;
+  counter: number = 0;
+  targetSectionOffset: number = 300;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (scrollPosition >= this.targetSectionOffset) {
+      this.startCounter();
+    }
+  }
+
+  startCounter(): void {
+    const interval = setInterval(() => {
+      if (this.counter < 150) {
+        this.counter++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 50); // You can adjust the interval based on your preference
+  }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
