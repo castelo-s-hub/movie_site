870debdf5da3867ced9e1bb875d47514f9b6af3c
KAVI <kavindu123450@gmail.com>
2023-11-06 21:56:36 +0530

    section 5

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -1,5 +1,6 @@
-import {Component, HostListener, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, HostListener, Renderer2, ViewEncapsulation} from '@angular/core';
 import VanillaTilt from "vanilla-tilt";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-about',
@@ -18,11 +19,50 @@ export class AboutComponent {
   sec2counter: number = 250;
   sec4counter: number = 1500;
 
-  constructor() {
+  currentIndex = 0;
+  carouselItems = [
+    {
+      image: 'https://template.wphix.com/dhora-prv/dhora/assets/img/play/img-1.jpg',
+      title: 'Highway',
+      tag: 'Adventure / 182 Mins'
+    },
+    {
+      image: 'https://template.wphix.com/dhora-prv/dhora/assets/img/play/img-2.jpg',
+      title: 'Stallion',
+      tag: 'Adventure / 182 Mins'
+    },
+    {
+      image: 'https://template.wphix.com/dhora-prv/dhora/assets/img/play/img-3.jpg',
+      title: 'Warrior',
+      tag: 'Adventure / 182 Mins'
+    },
+    {
+      image: 'https://template.wphix.com/dhora-prv/dhora/assets/img/play/img-4.jpg',
+      title: 'Dark',
+      tag: 'Adventure / 182 Mins'
+    },
+    {
+      image: 'https://template.wphix.com/dhora-prv/dhora/assets/img/play/img-5.jpg',
+      title: 'Future Destiny',
+      tag: 'Adventure / 182 Mins'
+    }
+  ]; // Your carousel items
+  currentIndex2 = 0;
+  isDragging = false;
+  startPosition = 0;
+  offset = 0;
+  itemWidth = 360; // Width of each carousel item
+  margin = 15; // Margin between carousel items
+
+  constructor(private sanitizer: DomSanitizer, private cdr: ChangeDetectorRef, private elementRef: ElementRef, private renderer: Renderer2) {
   }
 
   ngOnInit() {
     this.vanillaTiltAnimation()
+
+    const totalWidth = this.carouselItems.length * (this.itemWidth + this.margin);
+    const carousal = document.querySelector('.carousel-container') as HTMLElement;
+    carousal.style.width = totalWidth + 'px';
   }
 
   @HostListener('window:scroll', ['$event'])
@@ -96,6 +136,39 @@ export class AboutComponent {
       glare: false,
       'max-glare': 0.5
     });
+  }
+
+  onMouseDown(event: MouseEvent): void {
+    this.isDragging = true;
+    this.startPosition = event.clientX - this.offset;
+  }
+
+  onMouseMove(event: MouseEvent): void {
+    if (this.isDragging) {
+      this.offset = event.clientX - this.startPosition;
+      this.cdr.detectChanges();
+      this.updateCarouselPosition();
+    }
+  }
+
+  onMouseUp(): void {
+    this.isDragging = false;
+    // Calculate the new current index based on the offset
+    this.currentIndex2 = Math.round(this.offset / (this.itemWidth + this.margin));
+    this.cdr.detectChanges();
+    this.updateCarouselPosition();
+  }
+
+  updateCarouselPosition(): void {
+    const minOffset = 0;
+    const maxOffset = -((this.carouselItems.length - 1) * (this.itemWidth + this.margin));
+
+    // Ensure the offset stays within the bounds of the carousel
+    this.offset = Math.max(maxOffset, Math.min(minOffset, this.offset));
+
+    // Calculate the new current index based on the offset
+    this.currentIndex2 = Math.round(this.offset / (this.itemWidth + this.margin));
+    this.cdr.detectChanges();
   }
 
 }
