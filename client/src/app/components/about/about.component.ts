7fbf222b1c9e40947000b85cef6cd8aa2ae84281
KAVI <kavindu123450@gmail.com>
2023-11-06 19:05:10 +0530

    section 2 and section 3 done

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -1,4 +1,6 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, HostListener, Renderer2, ViewEncapsulation} from '@angular/core';
+import {DomSanitizer} from "@angular/platform-browser";
+import VanillaTilt from "vanilla-tilt";
 
 @Component({
   selector: 'app-about',
@@ -9,9 +11,48 @@ import {Component, ViewEncapsulation} from '@angular/core';
 export class AboutComponent {
 
   isMenuOpen: boolean = false;
+  counter: number = 0;
+  sec2counter: number = 300;
+
+  constructor(private sanitizer: DomSanitizer, private cdr: ChangeDetectorRef, private elementRef: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngOnInit() {
+    this.vanillaTiltAnimation()
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (scrollPosition >= this.sec2counter) {
+      this.startCounter();
+    }
+  }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  startCounter(): void {
+    const interval = setInterval(() => {
+      if (this.counter < 150) {
+        this.counter++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 50); // You can adjust the interval based on your preference
+  }
+
+  vanillaTiltAnimation() {
+    const element = document.querySelector('.sec-3-image') as HTMLElement;
+
+    VanillaTilt.init(element, {
+      max: 25,
+      speed: 400,
+      glare: false,
+      'max-glare': 0.5
+    });
   }
 
 }
