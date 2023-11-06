89573454974383f8a85611996080a17330663a63
KAVI <kavindu123450@gmail.com>
2023-11-06 21:03:31 +0530

    section 4

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -1,5 +1,4 @@
-import {ChangeDetectorRef, Component, ElementRef, HostListener, Renderer2, ViewEncapsulation} from '@angular/core';
-import {DomSanitizer} from "@angular/platform-browser";
+import {Component, HostListener, ViewEncapsulation} from '@angular/core';
 import VanillaTilt from "vanilla-tilt";
 
 @Component({
@@ -12,9 +11,14 @@ export class AboutComponent {
 
   isMenuOpen: boolean = false;
   counter: number = 0;
-  sec2counter: number = 300;
+  counter1: number = 0;
+  counter2: number = 0;
+  counter3: number = 0;
+  counter4: number = 0;
+  sec2counter: number = 250;
+  sec4counter: number = 1500;
 
-  constructor(private sanitizer: DomSanitizer, private cdr: ChangeDetectorRef, private elementRef: ElementRef, private renderer: Renderer2) {
+  constructor() {
   }
 
   ngOnInit() {
@@ -24,9 +28,14 @@ export class AboutComponent {
   @HostListener('window:scroll', ['$event'])
   onScroll(event: Event): void {
     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    // console.log(scrollPosition)
 
     if (scrollPosition >= this.sec2counter) {
       this.startCounter();
+    }
+
+    if (scrollPosition >= this.sec4counter) {
+      this.startSec4Counter();
     }
   }
 
@@ -42,6 +51,40 @@ export class AboutComponent {
         clearInterval(interval);
       }
     }, 50); // You can adjust the interval based on your preference
+  }
+
+  startSec4Counter(): void {
+    const interval1 = setInterval(() => {
+      if (this.counter1 < 200) {
+        this.counter1++;
+      } else {
+        clearInterval(interval1);
+      }
+    }, 50);
+
+    const interval2 = setInterval(() => {
+      if (this.counter2 < 20) {
+        this.counter2++;
+      } else {
+        clearInterval(interval2);
+      }
+    }, 500);
+
+    const interval3 = setInterval(() => {
+      if (this.counter3 < 10) {
+        this.counter3++;
+      } else {
+        clearInterval(interval3);
+      }
+    }, 700);
+
+    const interval4 = setInterval(() => {
+      if (this.counter4 < 500) {
+        this.counter4++;
+      } else {
+        clearInterval(interval4);
+      }
+    }, 40);
   }
 
   vanillaTiltAnimation() {
