37b1b08484e606875b94c665dbf81237391198ee
kavi castelo <kavindu123450@gmail.com>
2023-11-08 19:14:20 +0530

    blog page init and main banner. update about page top bar

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {AboutComponent} from "./components/about/about.component";
+import {BlogComponent} from "./components/blog/blog.component";
 
 const routes: Routes = [
   { path:'', redirectTo:'/home', pathMatch:'full'},
   { path:'home', component: HomeComponent },
-  { path:'about', component: AboutComponent}
+  { path:'about', component: AboutComponent},
+  { path:'blog', component: BlogComponent}
 ];
 
 @NgModule({
