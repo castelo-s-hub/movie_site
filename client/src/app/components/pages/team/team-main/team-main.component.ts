3c70b245ccada3ecf5f1e295e58597f2a7700a67
kavi castelo <kavindu123450@gmail.com>
2023-11-22 20:50:32 +0530

    Team main page dummy done

--- client/src/app/components/pages/team/team-main/team-main.component.ts
+++ client/src/app/components/pages/team/team-main/team-main.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class TeamMainComponent {
 
+  cardList = [
+    {
+      name: 'Adward collin',
+      post: 'Flim Assistant',
+      img: 'https://template.wphix.com/dhora-prv/dhora/assets/img/team/img-1.jpg',
+      fb: 'www.facebook.com',
+      tw: 'www.twitter.com',
+      inst: 'www.instagram.com'
+    },
+    {
+      name: 'Adward collin',
+      post: 'Flim Assistant',
+      img: 'https://template.wphix.com/dhora-prv/dhora/assets/img/team/img-1.jpg',
+      fb: 'www.facebook.com',
+      tw: 'www.twitter.com',
+      inst: 'www.instagram.com'
+    }
+  ]
+
 }
