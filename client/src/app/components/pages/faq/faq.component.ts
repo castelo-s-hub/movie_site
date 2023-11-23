eab8bc8643627f629ed0d7afff35d9b8cb9be28a
kavi castelo <kavindu123450@gmail.com>
2023-11-23 18:14:13 +0530

    faq right side

--- client/src/app/components/pages/faq/faq.component.ts
+++ client/src/app/components/pages/faq/faq.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-faq',
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class FaqComponent {
 
+  isMenuOpen: boolean = false;
+  isPagesTabOpen: boolean = false;
+
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event): void {
+    let target = event.target as HTMLElement;
+
+    if (!target.matches("a")) {
+      this.isPagesTabOpen = false;
+      this.cdr.detectChanges();
+    }
+  }
+
+  ngOnInit() {
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  pagesTabOpen() {
+    this.isPagesTabOpen = !this.isPagesTabOpen;
+  }
 }
